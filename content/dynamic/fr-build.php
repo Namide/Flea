3a1765793523bc39c96aad4edb5f62444101009b
Namide <damien.git@gmail.com>
2014-11-20 18:35:08 +0100

    Delete old design

--- content/dynamic/fr-build.php
+++
@@ -1,9 +0,0 @@
-
-<h1>Dynamic page</h1>
-
-<?php
-
-var_dump( Flea::getGeneral()->getCurrentGetUrl() );//Flea\General::getInstance()->getCurrentGetUrl() );
-
- //if ( Flea\BuildUtil::getInstance()-> )
-	 
